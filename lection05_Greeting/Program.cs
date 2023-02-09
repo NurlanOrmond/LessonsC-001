5ecf1e45e257facd369ba2f59ee1a0d2dfac22af
Nurlan Ormond <sunshineforester@gmail.com>
2023-02-09 17:49:41 +0300

    lection05_Condition

---
+++ lection05_Greeting/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine ("What's your name?");
+string name = Console.ReadLine ();
+
+if (name.ToLower() == "james") {
+    Console.WriteLine ("Look, that's James Bond!");
+}
+else {
+    Console.Write ("Hello, " + name);
+}
